f306d0a6ca541fb66e9b653b496582ef4260dc67
whjkd2005 <59304699+whjkd2005@users.noreply.github.com>
2021-03-28 18:39:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+
 # Modify default IP
 sed -i 's/192.168.1.1/172.16.3.253/g' package/base-files/files/bin/config_generate
